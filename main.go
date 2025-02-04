a3483ed4a69557bd82f4c90d8dfa7021f9fddce5
Roman Reshetko <rvreshetko@edu.hse.ru>
2025-02-04 12:31:59 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"cv-auth-backend/database"
+	"cv-auth-backend/handlers"
+	"github.com/rs/cors"
+	"log"
+	"net/http"
+)
+
+func main() {
+	err := database.ConnectDB()
+	if err != nil {
+		return
+	}
+	defer database.DB.Close()
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/signup", handlers.SignUpHandler)
+	mux.HandleFunc("/login", handlers.LogInHandler)
+
+	corsHandler := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:5173"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Content-Type", "Authorization"},
+		AllowCredentials: true,
+	})
+	handler := corsHandler.Handler(mux)
+	log.Fatal(http.ListenAndServe(":8080", handler))
+}
